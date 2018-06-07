cbfeca28cd6f8f9d4d63a29c229cb17a88b3d5e1
Thomas Hohnke <thohnke@eagles.bridgewater.edu>
2018-06-07 09:14:53 -0400

    Update DistViewOld.sh

--- ClassFiles/Shell Scripts/DistViewOld.sh
+++ ClassFiles/Shell Scripts/DistViewOld.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
 
+# This script allows you to view your linux distribution, and some other info
+# Author - Thomas Hohnke
+# Version - V1
+
+read -p "Would you like to know your Linux distro?" ans
+if [ $ans == "Y" ] || [ $ans == "y" ]; then
+   cat /etc/*-release
+else
+   exit 0
+fi
