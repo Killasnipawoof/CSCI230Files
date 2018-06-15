e47b06841003d24a71e3588ff2b190f53d36aaa6
killasnipawoof <killasnipawoofgamer@hotmail.com>
2018-06-15 11:36:53 -0400

    Added a description of what the script does

--- ClassFiles/Shell Scripts/D620WifiInstaller.sh
+++ ClassFiles/Shell Scripts/D620WifiInstaller.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# This shell script will enable the wifi radio chipsets on laptops utilizing a broadcom chipset
+# These Laptops include:
+# Dell Latitude D620
+# Author - Thomas Hohnke
+# Version - V1 2018
 
 sudo apt-get update;
 sudo apt-get install -y firmware-b43-installer;
