d331d759284d6b635f1cc4c06099a8ab10c56b84
Thomas <killasnipawoofgamer@hotmail.com>
2018-06-17 15:51:42 -0400

    Modified the Syntax of these scripts

--- ClassFiles/Shell Scripts/sftpselectorefficent.sh
+++ ClassFiles/Shell Scripts/sftpselectorefficent.sh
@@ -4,22 +4,22 @@ echo -e "Please enter the number of the server you wish to connect to\n1. Lab Se
 read num
 
 	if [ $num == 1 ]; then
-		read -p "What's your username?" servuser
+		read -p "What's your username?: " servuser
 		echo "Welcome, $servuser, to the BC3 mainframe."
 		sftp $servuser@bc3.bridgewater.edu
 
 	elif [ $num == 2 ]; then
-		read -p "What's your BC username?" bcuser
+		read -p "What's your BC username?: " bcuser
 		echo "Welcome, $bcuser, to the Bridgewater College CS Department server."    
 		sftp $bcuser@cs.bridgewater.edu
 
 	elif [ $num == 3 ]; then
-		read -p "Username?" user
+		read -p "Username?: " user
 		echo "Welcome, $user, to Thomas's Archlinux deluxe server!"
 		sftp $user@192.168.1.38
 
 	elif [ $num == 4 ]; then
-		read -p "Username?" user
+		read -p "Username?: " user
 		echo "Welcome $user, to Thomas's Archlinux deluxe server!"
 		sftp $user@71.63.37.207
 
