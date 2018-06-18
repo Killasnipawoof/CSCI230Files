4a301bd77b1a5b52ef4d257f6f64794fc989932c
Thomas <killasnipawoofgamer@hotmail.com>
2018-06-17 21:11:18 -0400

    Added new Languages

--- ClassFiles/Shell Scripts/EnvironmentInstaller.sh
+++ ClassFiles/Shell Scripts/EnvironmentInstaller.sh
@@ -16,4 +16,14 @@ sudo apt-get install ruby-full -y;
 #install the R Language package
 sudo apt-get install r-base -y;
 
+#Install the Rust Language and it's dependency
+sudo apt-get install curl -y; curl https://sh.rustup.rs -sSf | sh;
+
+#Install monodevelop, the C# compiler for Linux
+sudo apt install apt-transport-https dirmngr -y;
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF;
+echo "deb https://download.mono-project.com/repo/ubuntu vs-bionic main" | sudo tee /etc/apt/sources.list.d/mono-official-vs.list;
+sudo apt update;
+sudo apt-get install monodevelop -y;
+
 
