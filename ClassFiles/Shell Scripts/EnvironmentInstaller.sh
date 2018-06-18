927324c957909d326e56e6dcc1845e5d42f9baa6
Thomas <killasnipawoofgamer@hotmail.com>
2018-06-17 20:41:45 -0400

    Added additional languages

--- ClassFiles/Shell Scripts/EnvironmentInstaller.sh
+++ ClassFiles/Shell Scripts/EnvironmentInstaller.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#Update the package repository
+sudo apt-get update;
+
 #Install the Java JRE and JDK 
 sudo apt-get install default-jre -y;
 sudo apt-get install default-jdk -y;
@@ -10,6 +13,7 @@ sudo apt-get install python3 -y;
 #Install the Ruby Interpreter
 sudo apt-get install ruby-full -y;
 
-
+#install the R Language package
+sudo apt-get install r-base -y;
 
 
