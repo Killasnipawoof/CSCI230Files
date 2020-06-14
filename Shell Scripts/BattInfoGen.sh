2cdb5754c8db8329ebed9351e160d6c489139ffa
killasnipawoof <killasnipawoofgamer@hotmail.com>
2020-06-14 18:19:37 -0400

    Changed BattInfoGen to enable custom log file naming

--- Shell Scripts/BattInfoGen.sh
+++ Shell Scripts/BattInfoGen.sh
@@ -5,7 +5,8 @@
 upower -i /org/freedesktop/UPower/devices/battery_BAT0
 read -p "Save Results? " ans
 if [ $ans == y ] || [ $ans == Y ]; then
-       upower -i /org/freedesktop/UPower/devices/battery_BAT0 > BatInfo.txt
+	read -p "Name?: " batinfo
+	upower -i /org/freedesktop/UPower/devices/battery_BAT0 > $batinfo.txt
 else
  exit 0
 fi 
